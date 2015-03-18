2a48ff7e7cc6aaf28f554eb372b4e6b62c383d53
Jesse House <mail@jessehouse.com>
2015-03-18 15:02:14 -0700

    tweak failing test

--- test/models/foo_test.rb
+++ test/models/foo_test.rb
@@ -8,13 +8,16 @@ class FooTest < ActiveSupport::TestCase
 
   test "saves serialized empty hash" do
     bar = {}
-    assert Foo.new(title: "the-title", bar: bar).save
+    foo = Foo.new(title: "the-title", bar: bar)
+    assert foo.bar == {}
+    assert foo.save # BOOM! SQLite3::ConstraintException: NOT NULL constraint failed: foos.bar
   end
 
   test "cannot save without bar" do
     bar = nil
 
-    assert_raises do
+    assert_raises ActiveRecord::StatementInvalid  do
+      # really SQLite3::ConstraintException
       Foo.new(title: "the-title", bar: bar).save
     end
   end
